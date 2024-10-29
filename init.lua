db219bd5b5b639b0f88caa4f55077d89a230487d
Vitor Nesello <vitornesello@gmail.com>
2024-10-29 17:07:16 +0100

    plugins: add monkai themes

--- init.lua
+++ init.lua
@@ -347,6 +347,14 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'loctvl842/monokai-pro.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      require('monokai-pro').setup()
+    end,
+  },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
