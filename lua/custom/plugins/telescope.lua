527c7130429b4bd257d86b59fc9890795e9e4c44
Vitor Nesello <vitornesello@gmail.com>
2025-06-02 10:00:12 +0200

    nvim: telescope: fussy search word

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -89,12 +89,16 @@ return {
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Fuzzy [S]earch [W]ord under cursor' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<M-b>', builtin.buffers, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    local fuzzy_search_word = function()
+      return builtin.grep_string { shorten_path = true, word_match = '-w', only_sort_text = true, search = '' }
+    end
+    vim.keymap.set('n', '<leader>fs', fuzzy_search_word, { desc = '[F]uzzy [S]earch custom word' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
